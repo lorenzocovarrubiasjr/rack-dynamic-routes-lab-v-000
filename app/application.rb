ce283bf10d4508fd4ad92f94d3ef91d0ff6506dc
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-30 19:52:15 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+class Application
+  
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+    
+    search_term = req.params["q"]
+    
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if Item.all.find{|i| i.name == item_name}
+      item_we_have = Item.all.find{|i| i.name == item_name}
+      resp.write "#{item_we_have.price}"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end 
+    else   
+      resp.write "Route not found"
+      resp.status = 404
+    end 
+    
+    resp.finish 
+  end 
+  
+end 
